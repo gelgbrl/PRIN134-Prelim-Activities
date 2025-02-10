61e9109b594d4712e10bc73c111878039f34bb94
gelgbrl <amg33102@gmail.com>
2025-02-10 15:59:17 +0800

    Updated problem 2.

--- act2.js
+++ act2.js
@@ -4,29 +4,35 @@ let num3 = 0;
 
 function addition(num1, num2){
     let addNum = num1 + num2;
-    return addNum;
+    console.log(`Addition: 10 + 5 =`, addNum);
 }
 
 function subtraction(num1, num2) {
     let subNum = num1 - num2;
-    return subNum;
+    console.log(`Subtraction: 10 - 5 =`, subNum);
 }
 
 function multiplication(num1, num2){
     let multiNum = num1 * num2;
-    return multiNum;
+    console.log(`Multiplication: 10 * 5 =`, multiNum);
 }
 
 function division(num1, num2){
     let divNum = num1 / num2;
-    return divNum;
+    console.log(`Division: 10 / 5 =`, divNum);
 }
 
-function dvisionZero(num1, num3){
-    let divZero = num1 / num3;
-    return divZero;
+function divisionZero(num1, num3){
+    let divZero = num1 / num3; 
+    console.log(`Division by zero: 10 / 0 =`, divZero);
 }
 
 function calculator(){
-    
+    addition(num1, num2);
+    subtraction(num1, num2);
+    multiplication(num1, num2);
+    division(num1, num2);
+    divisionZero(num1, num3);
 }
+
+calculator();
