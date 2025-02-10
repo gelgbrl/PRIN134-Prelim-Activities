c0525a3f9fc001da58fc3f4d3c959133a653432f
gelgbrl <amg33102@gmail.com>
2025-02-10 15:26:26 +0800

    Updated problem 1.

--- act1.js
+++ act1.js
@@ -1,10 +1,11 @@
-let firstName = "Gelai";
-let lastName = "Gabriel";
-var gender = "Female";
+function greetings(firstName, lastName, gender){
+    if(gender == 'Female') {
+        console.log(`Greetings! MS. ${firstName} ${lastName}`)
+    }
+    else if(gender == 'Male'){
+        console.log(`Greetings! MR. ${firstName} ${lastName}`)
+    }
+}
 
-if(gender = 'Female') {
-    console.log(`Greetings! MS. ${firstName} ${lastName}`)
-}
-else if (gender = 'Male'){
-    console.log(`Greetings! MR. ${firstName} ${lastName}`)
-}
+greetings('Gelai', 'Gabriel', 'Female');
+greetings('Jon', 'Snow', 'Male')
